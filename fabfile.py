8d5f1f7a5d46e918920c94e82981faeb648d98c6
Joseph Long <josephoenix@gmail.com>
2014-03-28 14:26:03 -0700

    Split up upload and deploy in fabfile

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,9 @@ def prepare():
     local('git add -p && git commit')
     local('git push')
 
+def upload():
+    put('./output/*', '/srv/www/')
+
 def deploy():
     prepare()
-    put('./output/*', '/srv/www/')
+    upload()
