e17e0ca1d94857f85de37fb56813ca3befae6b10
Joseph Long <josephoenix@gmail.com>
2014-07-15 17:33:16 -0400

    Hide future employment; add skeleton-generating commmands; tweak homepage

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,12 @@
 from fabric.api import *
+import unicodedata
+import re
+import datetime
+
+def slugify(value):
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub('[^\w\s-]', '', value)
+    return re.sub('[-\s]+', '-', value).strip().lower()
 
 def prepare():
     local('git add -p')
@@ -13,3 +21,21 @@ def upload():
 def deploy():
     prepare()
     upload()
+
+def post():
+    slug_title = unicode(prompt("What is the short title for the post?"))
+    slug = slugify(slug_title)
+    full_slug = '{0}-{1}'.format(datetime.date.today().isoformat(), slug)
+    local('cp ./skeletons/post.md ./posts/{0}.md'.format(full_slug))
+    local('mkdir ./posts/{0}/'.format(full_slug))
+
+def portfolio():
+    slug_title = unicode(prompt("What's an unambiguous short name for this project?"))
+    slug = slugify(slug_title)
+    local('mkdir ./portfolio/{0}'.format(slug))
+    local('cp ./skeletons/portfolio.yml ./portfolio/{0}/project.yml'.format(slug))
+
+def experience():
+    slug_title = unicode(prompt("What's an unambiguous short name for this employer?"))
+    slug = slugify(slug_title)
+    local('cp ./skeletons/experience.yml ./experience/{0}.yml'.format(slug))
--- generate.py
+++ generate.py
@@ -12,7 +12,7 @@ import os, errno
 # A quick and dirty static site generator (kind of like Jekyll) to generate
 # static HTML for my personal site. Warning: this code is messy and not
 # for public consumption :)
-# 
+#
 # This code is public domain, should anyone want to attempt to repurpose it.
 # (I retain copyright of all content and the design of the site, except as
 # otherwise noted.)
@@ -60,46 +60,46 @@ for project in glob.glob(os.path.join(APP_ROOT, 'portfolio', '*', 'project.yml')
         portfolio[project_name] = yaml.safe_load(f)
         # YAML turns a double linebreak into a single one, breaking Markdown
         portfolio[project_name]['description'] = portfolio[project_name]['description'].replace('\n', '\n\n')
-    
+
     # generate portfolio pages
     ensure_dir(os.path.join(OUTPUT_ROOT, project_name))
     with open(os.path.join(OUTPUT_ROOT, project_name, 'index.html'), 'w') as f:
         f.write(project_template.render(**portfolio[project_name]))
-    
+
     if os.path.exists(os.path.join(project_dir, 'thumb.png')):
         shutil.copy2(
             os.path.join(project_dir, 'thumb.png'),
             os.path.join(OUTPUT_ROOT, project_name)
         )
         portfolio[project_name]['thumb'] = 'thumb.png'
-    
+
     # generate resized images
     thumb_size = 150, 150
     thumbs_dir = os.path.join(OUTPUT_ROOT, project_name, 'thumbs')
     ensure_dir(thumbs_dir)
-    
+
     default_size = 460, 460
-    
+
     fullsize_dir = os.path.join(OUTPUT_ROOT, project_name, 'full')
     ensure_dir(fullsize_dir)
-    
+
     for image in portfolio[project_name]['images']:
         thumbdest = os.path.join(thumbs_dir, image['filename'])
         defaultdest = os.path.join(OUTPUT_ROOT, project_name, image['filename'])
-        
+
         if os.path.exists(thumbdest) and os.path.exists(defaultdest):
             continue # don't regenerate if output files exist
-        
+
         im = Image.open(os.path.join(project_dir, image['filename']))
         thumb = im.copy()
         default = im.copy()
-        
+
         thumb.thumbnail(thumb_size, Image.ANTIALIAS)
         thumb.save(thumbdest)
-        
+
         default.thumbnail(default_size, Image.ANTIALIAS)
         default.save(defaultdest)
-        
+
         subprocess.call('cp -pv {src} {dst}'.format(
             src=os.path.join(project_dir, image['filename']),
             dst=fullsize_dir,
@@ -113,21 +113,21 @@ for post_path in glob.glob(os.path.join(APP_ROOT, 'posts', '*.md')):
     y, m, d = map(int, post_filename.split('-')[:3])
     post_date = datetime.date(y, m, d)
     post_slug = '-'.join(post_filename.split('-')[3:])[:-3] # chop off '.md'
-    
+
     print 'Found blog post', post_slug
-    
+
     post_data = open(post_path, 'r').read()
     post_meta, post_content = post_data.split('---')[1:]
     post = yaml.load(post_meta.strip())
     post['content'] = post_content.strip()
     post['date'] = post_date
     post['slug'] = post_slug
-    
+
     # generate individual post files
     ensure_dir(os.path.join(OUTPUT_ROOT, 'writing', post_slug))
     with open(os.path.join(OUTPUT_ROOT,  'writing', post_slug, 'index.html'), 'w') as f:
         f.write(post_template.render(**post))
-    
+
     # copy supporting files for post
     if os.path.isdir(post_path[:-3]): # chop off .md and check if there's a directory
         for post_file in glob.glob(os.path.join(APP_ROOT, 'posts', post_path[:-3], '*')):
@@ -169,7 +169,7 @@ for position_file in glob.glob(os.path.join(APP_ROOT, 'experience', '*.yml')):
 
 positions.sort()
 positions.reverse()
-positions = [data for datestamp, data in positions]
+positions = [data for datestamp, data in positions if datestamp < datetime.date.today()]
 
 # render the homepage
 
