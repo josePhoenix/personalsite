b363d8f7f46838d4d2be51938baf4b3d9bd1c932
Joseph Long <me@joseph-long.com>
2014-08-13 11:50:13 -0400

    Add template-based generated plain pages

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,7 @@ from fabric.api import *
 import unicodedata
 import re
 import datetime
+import os.path
 
 env.hosts = ['joseph-long.com']
 
@@ -42,3 +43,11 @@ def experience():
     slug_title = unicode(prompt("What's an unambiguous short name for this employer?"))
     slug = slugify(slug_title)
     local('cp ./skeletons/experience.yml ./experience/{0}.yml'.format(slug))
+
+def page():
+    slug_title = unicode(prompt("What's an unambiguous short name for this page?"))
+    slug = slugify(slug_title)
+    target = './pages/{0}.md'.format(slug)
+    if os.path.isfile(target):
+        raise RuntimeError("Destination file {0} exists already!".format(target))
+    local('cp ./skeletons/page.md {0}'.format(target))
